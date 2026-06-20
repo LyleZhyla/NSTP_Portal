a505dae6ebd3be84ffd17674c5b77cb763221719
LyleZhyla <elyl.patalod16@gmail.com>
2026-06-20 20:06:38 +0800

    Add files via upload

---
+++ config/mail.local.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'smtp.gmail.com',
+    'username' => 'tau.nstp@gmail.com',
+    'password' => 'rldqigjifxqhequs',
+    'port' => 587,
+    'encryption' => 'tls',
+    'from_email' => 'tau.nstp@gmail.com',
+    'from_name' => 'TAU NSTP Office',
+];
