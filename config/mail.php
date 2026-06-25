bd556520abe876b90f6e660eec668962e519483f
Lyle Zhyla <elyl.patalod16@gmail.com>
2026-06-25 13:48:20 +0800

    Add SMTP mail config

---
+++ config/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'smtp.gmail.com',
+    'username' => 'nstp2027@gmail.com',
+    'password' => 'wsfrpjrrkqyuocjr',
+    'port' => 587,
+    'encryption' => 'tls',
+    'from_email' => 'nstp2027@gmail.com',
+    'from_name' => 'TAU NSTP Office',
+];
